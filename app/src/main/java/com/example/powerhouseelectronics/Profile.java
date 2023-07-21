f2b7d07470ec8d996237c6b171632151c152300b
Ferdlcz <ferdlcskate@gmail.com>
2023-07-20 23:26:55 -0600

    Rol user puede modificar su informacion de perfil

--- app/src/main/java/com/example/powerhouseelectronics/Profile.java
+++ app/src/main/java/com/example/powerhouseelectronics/Profile.java
@@ -1,33 +1,66 @@
 package com.example.powerhouseelectronics;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class Profile extends AppCompatActivity {
 
     Toolbar toolbar;
-
+    Button btnDeleteAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button btnRedireccionEdit = findViewById(R.id.btnEditUser);
+
+        btnRedireccionEdit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Profile.this, UserEdit.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDeleteAcc = findViewById(R.id.btnDeleteAcc);
+        btnDeleteAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertConfirm();
+            }
+        });
 
 
         SharedPreferences sharedPreferences = getSharedPreferences("Token", MODE_PRIVATE);
@@ -51,6 +84,58 @@ public class Profile extends AppCompatActivity {
         Picasso.with(this)
                   .load(image)
                   .into(imageView);
+    }
+
+    private void AlertConfirm() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Eliminar cuenta");
+        builder.setMessage("¿Estás seguro de que deseas eliminar tu cuenta?");
+        builder.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Si el usuario confirma la eliminación, realiza la solicitud al servidor
+                // para eliminar la cuenta y cierra la sesión del usuario
+                deleteUserAccount();
+            }
+        });
+        builder.setNegativeButton("Cancelar", null);
+        builder.show();
+    }
+
+    private void deleteUserAccount() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Token", MODE_PRIVATE);
+        String modifierId = sharedPreferences.getString("id", "");
+
+        OkHttpClient client = new OkHttpClient();
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+        String jsonBody = "{\"modifierId\":\"" + modifierId + "\"}";
+
+        RequestBody requestBody = RequestBody.create(jsonBody, JSON);
+
+        Request request = new Request.Builder()
+                .url("http://173.255.204.68/api/users/" + modifierId)
+                .delete(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("USER_DELETE", "Usuario eliminado correctamente");
+                } else {
+                    Log.d("USER_DELETE", "Error al eliminar usuario: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d("USER_DELETE", "Error en la solicitud: " + e.getMessage());
+            }
+        });
+
+        logout();
     }
 
         @Override
---
+++ app/src/main/java/com/example/powerhouseelectronics/UserEdit.java
@@ -0,0 +1,173 @@
+package com.example.powerhouseelectronics;
+//usuarios normales
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class UserEdit extends AppCompatActivity {
+
+    Toolbar toolbar;
+    String modifierId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_edit);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Token", MODE_PRIVATE);
+        modifierId = sharedPreferences.getString("id", "");
+
+        String name = sharedPreferences.getString("name", "");
+        String email = sharedPreferences.getString("email", "");
+        String image = sharedPreferences.getString("image", "");
+        String address = sharedPreferences.getString("address", "");
+        String phone = sharedPreferences.getString("phone", "");
+
+        EditText nameEditText = findViewById(R.id.editTextName);
+        nameEditText.setText(name);
+
+        EditText emailEditText = findViewById(R.id.editTextEmail);
+        emailEditText.setText(email);
+
+        EditText phoneEditText = findViewById(R.id.editTextPhone);
+        phoneEditText.setText(phone);
+
+        EditText addressEditText = findViewById(R.id.editTextAddress);
+        addressEditText.setText(address);
+
+        // Display user image using Picasso
+        ImageView imageView = findViewById(R.id.imageView4);
+        Picasso.with(this)
+                .load(image)
+                .into(imageView);
+
+        Button btnUpdateUser = findViewById(R.id.btnUpdateUser);
+        btnUpdateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserInformation();
+            }
+        });
+    }
+
+    private void updateUserInformation() {
+
+        EditText nameEditText = findViewById(R.id.editTextName);
+        String newName = nameEditText.getText().toString();
+
+        EditText emailEditText = findViewById(R.id.editTextEmail);
+        String newEmail = emailEditText.getText().toString();
+
+        EditText phoneEditText = findViewById(R.id.editTextPhone);
+        String newPhone = phoneEditText.getText().toString();
+
+        EditText addressEditText = findViewById(R.id.editTextAddress);
+        String newAddress = addressEditText.getText().toString();
+
+
+        String jsonBody = "{"
+                + "\"name\":\"" + newName + "\","
+                + "\"email\":\"" + newEmail + "\","
+                + "\"phone\":\"" + newPhone + "\","
+                + "\"address\":\"" + newAddress + "\","
+                + "\"modifierId\":\"" + modifierId + "\""
+                + "}";
+
+        OkHttpClient client = new OkHttpClient();
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        RequestBody requestBody = RequestBody.create(jsonBody, JSON);
+
+        Request request = new Request.Builder()
+                .url("http://173.255.204.68/api/users/" + modifierId)
+                .put(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("USER_UPDATE", "Actualización exitosa");
+                    finish();
+                } else {
+                    Log.d("USER_UPDATE", "Error al actualizar: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d("USER_UPDATE", "Error en la solicitud: " + e.getMessage());
+            }
+        });
+
+        Log.d("USER_UPDATE", "JSON Request Body: " + jsonBody);
+        Log.d("USER_UPDATE", "URL: " + request.url());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cliente, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_logout) {
+            logout();
+            return false;
+        } else if (item.getItemId() == R.id.profile) {
+            GoProfile();
+            return false;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void removeTokenFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Token", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("token");
+        editor.apply();
+    }
+
+    private void logout() {
+        removeTokenFromSharedPreferences();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void GoProfile() {
+        Intent intent = new Intent(UserEdit.this, Profile.class);
+        startActivity(intent);
+    }
+}
