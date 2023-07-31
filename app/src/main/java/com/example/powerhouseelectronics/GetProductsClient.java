7e2cbeb66fabe4064eeea2da5722d8ba7bba3861
Ferdlcz <ferdlcskate@gmail.com>
2023-07-30 23:02:15 -0600

    Vistas de productos y filtros funcionando en parte de cliente

---
+++ app/src/main/java/com/example/powerhouseelectronics/GetProductsClient.java
@@ -0,0 +1,233 @@
+package com.example.powerhouseelectronics;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+import com.squareup.picasso.Picasso;
+
+public class GetProductsClient {
+    public static CardView createCardPhone(Context context, Index.Phone phone){
+
+        CardView PhoneCardView = new CardView(context);
+        LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        cardParams.setMargins(20, 20, 20, 20);
+        PhoneCardView.setLayoutParams(cardParams);
+        PhoneCardView.setRadius(4);
+        PhoneCardView.setCardElevation(10);
+
+        LinearLayout cardLayout = new LinearLayout(context);
+        cardLayout.setOrientation(LinearLayout.VERTICAL);
+        PhoneCardView.addView(cardLayout);
+
+        ImageView imageView = new ImageView(context);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                400
+        );
+
+        imageView.setLayoutParams(imageParams);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        Picasso.with(context)
+                .load(phone.getImage())
+                .into(imageView);
+
+        cardLayout.addView(imageView);
+
+        TextView brandTextView = new TextView(context);
+        brandTextView.setText("Marca: " + phone.getBrand());
+        brandTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(brandTextView);
+
+        TextView modelTextView = new TextView(context);
+        modelTextView.setText("Modelo: " + phone.getModel());
+        modelTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(modelTextView);
+
+        TextView colorTextView = new TextView(context);
+        colorTextView.setText("Color: " + phone.getColor());
+        colorTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(colorTextView);
+
+        TextView storageTextView = new TextView(context);
+        storageTextView.setText("Almacenamiento: " + phone.getStorage());
+        storageTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(storageTextView);
+
+        TextView priceTextView = new TextView(context);
+        priceTextView.setText("Precio: " + phone.getPrice());
+        priceTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(priceTextView);
+
+        TextView screenresTextView = new TextView(context);
+        screenresTextView.setText("Resolucion de pantalla: " + phone.getScreenResolution());
+        screenresTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(screenresTextView);
+
+        TextView cameraTextView = new TextView(context);
+        cameraTextView.setText("Camara: " + phone.getCameraResolution());
+        cameraTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(cameraTextView);
+
+        TextView stockTextView = new TextView(context);
+        stockTextView.setText("Cantidad: " + phone.getStock());
+        stockTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(stockTextView);
+
+        return PhoneCardView;
+    }
+
+
+    //CARDS DE COMPUTADORAS
+    public static CardView createCardCpu(Context context, Index.CPU cpu){
+
+        CardView CpuCardView = new CardView(context);
+        LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        cardParams.setMargins(20, 20, 20, 20);
+        CpuCardView.setLayoutParams(cardParams);
+        CpuCardView.setRadius(4);
+        CpuCardView.setCardElevation(10);
+
+        LinearLayout cardLayout = new LinearLayout(context);
+        cardLayout.setOrientation(LinearLayout.VERTICAL);
+        CpuCardView.addView(cardLayout);
+
+        ImageView imageView = new ImageView(context);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                400
+        );
+
+        imageView.setLayoutParams(imageParams);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        Picasso.with(context)
+                .load(cpu.getImage())
+                .into(imageView);
+
+        cardLayout.addView(imageView);
+
+        TextView CpubrandTextView = new TextView(context);
+        CpubrandTextView.setText("Marca: " + cpu.getBrand());
+        CpubrandTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpubrandTextView);
+
+        TextView CpumodelTextView = new TextView(context);
+        CpumodelTextView.setText("Modelo: " + cpu.getModel());
+        CpumodelTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpumodelTextView);
+
+        TextView CpuProcessorTextView = new TextView(context);
+        CpuProcessorTextView.setText("Procesador: " + cpu.getProcessor());
+        CpuProcessorTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuProcessorTextView);
+
+        TextView CpuRamTextView = new TextView(context);
+        CpuRamTextView.setText("Memoria Ram: " + cpu.getRam());
+        CpuRamTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuRamTextView);
+
+        TextView CpuStorageTextView = new TextView(context);
+        CpuStorageTextView.setText("Almacenamiento: " + cpu.getStorage());
+        CpuStorageTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuStorageTextView);
+
+        TextView CpuPriceTextView = new TextView(context);
+        CpuPriceTextView.setText("Precio: " + cpu.getPrice());
+        CpuPriceTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuPriceTextView);
+
+        TextView CpuOperatingSystemTextView = new TextView(context);
+        CpuOperatingSystemTextView.setText("Sistema Operativo: " + cpu.getOperatingSystem());
+        CpuOperatingSystemTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuOperatingSystemTextView);
+
+        TextView CpuGraphicsCardTextView = new TextView(context);
+        CpuGraphicsCardTextView.setText("Tarjeta Grafica: " + cpu.getGraphicsCard());
+        CpuGraphicsCardTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuGraphicsCardTextView);
+
+        TextView CpuStockTextView = new TextView(context);
+        CpuStockTextView.setText("Cantidad: " + cpu.getStock());
+        CpuStockTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(CpuStockTextView);
+
+        return CpuCardView;
+    }
+
+    public static CardView createCardConsole(Context context, Index.Console console) {
+        CardView ConsoleCardView = new CardView(context);
+        LinearLayout.LayoutParams cardParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        cardParams.setMargins(20, 20, 20, 20);
+        ConsoleCardView.setLayoutParams(cardParams);
+        ConsoleCardView.setRadius(4);
+        ConsoleCardView.setCardElevation(10);
+
+        LinearLayout cardLayout = new LinearLayout(context);
+        cardLayout.setOrientation(LinearLayout.VERTICAL);
+        ConsoleCardView.addView(cardLayout);
+
+        ImageView imageView = new ImageView(context);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                400
+        );
+
+        imageView.setLayoutParams(imageParams);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        Picasso.with(context)
+                .load(console.getImage())
+                .into(imageView);
+
+        cardLayout.addView(imageView);
+
+        TextView ConsolebrandTextView = new TextView(context);
+        ConsolebrandTextView.setText("Marca: " + console.getBrand());
+        ConsolebrandTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsolebrandTextView);
+
+        TextView ConsolemodelTextView = new TextView(context);
+        ConsolemodelTextView.setText("Modelo: " + console.getModel());
+        ConsolemodelTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsolemodelTextView);
+
+        TextView ConsoleStorageTextView = new TextView(context);
+        ConsoleStorageTextView.setText("Almacenamiento: " + console.getStorage());
+        ConsoleStorageTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsoleStorageTextView);
+
+        TextView ConsolePriceTextView = new TextView(context);
+        ConsolePriceTextView.setText("Precio: " + console.getPrice());
+        ConsolePriceTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsolePriceTextView);
+
+        TextView ConsoleFeaturesTextView = new TextView(context);
+        ConsoleFeaturesTextView.setText("Características: " + console.getFeatures());
+        ConsoleFeaturesTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsoleFeaturesTextView);
+
+        TextView ConsoleColorTextView = new TextView(context);
+        ConsoleColorTextView.setText("Color: " + console.getColor());
+        ConsoleColorTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsoleColorTextView);
+
+        TextView ConsoleStockSystemTextView = new TextView(context);
+        ConsoleStockSystemTextView.setText("Cantidad: " + console.getStock());
+        ConsoleStockSystemTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+        cardLayout.addView(ConsoleStockSystemTextView);
+
+        return ConsoleCardView;
+    }
+}
--- app/src/main/java/com/example/powerhouseelectronics/Index.java
+++ app/src/main/java/com/example/powerhouseelectronics/Index.java
@@ -3,22 +3,48 @@ package com.example.powerhouseelectronics;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Index extends AppCompatActivity {
 
     Toolbar toolbar;
+    LinearLayout productLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +54,17 @@ public class Index extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        productLayout = findViewById(R.id.product_layout);
+
         SharedPreferences sharedPreferences = getSharedPreferences("Token", MODE_PRIVATE);
         String userName = sharedPreferences.getString("name", "Nombre de usuario");
         TextView userNameTextView = findViewById(R.id.user_name);
         userNameTextView.setText(userName);
+
+        Button btnViewPhone = findViewById(R.id.btnViewTelefono);
+        Button btnViewConsole = findViewById(R.id.btnViewConsola);
+        Button btnViewCpu = findViewById(R.id.btnViewPC);
+
 
         CircleImageView userProfileImageView = findViewById(R.id.user_profile_image);
 
@@ -40,8 +73,468 @@ public class Index extends AppCompatActivity {
         Picasso.with(this)
                 .load(userImageURL)
                 .into(userProfileImageView);
+
+        btnViewPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Index.this, ViewCellPhones.class);
+                startActivity(intent);
+            }
+        });
+
+        btnViewConsole.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Index.this, ViewConsoles.class);
+                startActivity(intent);
+            }
+        });
+
+        btnViewCpu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Index.this, ViewComputers.class);
+                startActivity(intent);
+            }
+        });
+
+        LoadProducts();
     }
 
+
+    private void LoadProducts() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://173.255.204.68/api/products")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String jsonData = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mostrarProducts(jsonData);
+                        }
+                    });
+                } else {
+                    // Handle error response
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                // Handle failure
+            }
+        });
+    }
+
+    private void mostrarProducts(String jsonData) {
+        Gson gson = new Gson();
+        Index.ProductResponseList productResponseList = gson.fromJson(jsonData, Index.ProductResponseList.class);
+
+        List<Index.Product> productList = new ArrayList<>();
+
+        if (productResponseList != null) {
+            List<Index.Phone> phones = productResponseList.getPhones();
+            List<Index.CPU> cpus = productResponseList.getCpus();
+            List<Index.Console> consoles = productResponseList.getConsoles();
+
+            if (phones != null) {
+                productList.addAll(phones);
+            }
+
+            if (cpus != null) {
+                productList.addAll(cpus);
+            }
+
+            if (consoles != null) {
+                productList.addAll(consoles);
+            }
+        }
+
+        if (!productList.isEmpty()) {
+            for (Index.Product product : productList) {
+                String productJson = gson.toJson(product);
+                Log.d("PRODUCTS_JSON", productJson);
+
+                CardView productCard = createProductCard(product);
+                productLayout.addView(productCard);
+            }
+        } else {
+            Log.d("PRODUCTS_JSON", "No hay productos disponibles");
+        }
+    }
+
+    private CardView createProductCard(Index.Product product) {
+        if (product instanceof Index.Phone) {
+            return GetProductsClient.createCardPhone(this, (Index.Phone) product);
+        } else if (product instanceof Index.CPU) {
+            return GetProductsClient.createCardCpu(this, (Index.CPU) product);
+        } else if (product instanceof Index.Console) {
+            return GetProductsClient.createCardConsole(this, (Index.Console) product);
+        } else {
+            return new CardView(this);
+        }
+    }
+
+    private void LoadPhones() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://173.255.204.68/api/products")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String jsonData = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mostrarPhones(jsonData);
+                        }
+                    });
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void mostrarPhones(String jsonData) {
+        Gson gson = new Gson();
+        Index.ProductResponseList phoneResponseList = gson.fromJson(jsonData, Index.ProductResponseList.class);
+
+        List<Index.Phone> phoneList = phoneResponseList.getPhones();
+        if (phoneList != null && !phoneList.isEmpty()) {
+            for (Index.Phone phone : phoneList) {
+                String phoneJson = gson.toJson(phone);
+                Log.d("PHONES_JSON", phoneJson);
+
+                CardView phoneCard = GetProductsClient.createCardPhone(this, phone);
+                productLayout.addView(phoneCard);
+            }
+        } else {
+            Log.d("PHONES_JSON", "No hay telefonos disponibles");
+        }
+    }
+
+
+    private void LoadCpus() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://173.255.204.68/api/products")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String jsonData = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mostrarCpus(jsonData);
+                        }
+                    });
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void mostrarCpus(String jsonData) {
+        Gson gson = new Gson();
+        Index.ProductResponseList cpuResponseList = gson.fromJson(jsonData, Index.ProductResponseList.class);
+
+        List<Index.CPU> cpuList = cpuResponseList.getCpus();
+        if (cpuList != null && !cpuList.isEmpty()) {
+            for (Index.CPU cpu : cpuList) {
+                String cpuJson = gson.toJson(cpu);
+                Log.d("CPUS_JSON", cpuJson);
+
+                CardView cpuCard = GetProductsClient.createCardCpu(this, cpu);
+                productLayout.addView(cpuCard);
+            }
+        } else {
+            Log.d("CPUS_JSON", "No hay computadoras disponibles");
+        }
+    }
+
+    private void LoadConsoles(){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://173.255.204.68/api/products")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String jsonData = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mostrarConsoles(jsonData);
+                        }
+                    });
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void mostrarConsoles(String jsonData) {
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(new TypeToken<List<String>>() {}.getType(), new Index())
+                .create();
+
+        Index.ProductResponseList consoleResponseList = gson.fromJson(jsonData, Index.ProductResponseList.class);
+
+        List<Index.Console> consoleList = consoleResponseList.getConsoles();
+        if (consoleList != null && !consoleList.isEmpty()) {
+            for (Index.Console console : consoleList) {
+                String consoleJson = gson.toJson(console);
+                Log.d("CONSOLES_JSON", consoleJson);
+
+                CardView consoleCard = GetProductsClient.createCardConsole(this, console);
+                productLayout.addView(consoleCard);
+            }
+        } else {
+            Log.d("CONSOLES_JSON", "No hay consolas disponibles");
+        }
+    }
+
+
+    public class Product implements Serializable {
+        private String _id;
+        private String brand;
+        private String model;
+        private String storage;
+        private String image;
+        private String price;
+        private int stock;
+
+        public String get_id() {
+            return _id;
+        }
+
+        public void set_id(String _id) {
+            this._id = _id;
+        }
+
+        public String getBrand() {
+            return brand;
+        }
+
+        public void setBrand(String brand) {
+            this.brand = brand;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public void setImage(String image) {
+            this.image = image;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+        public String getStorage() {
+            return storage;
+        }
+
+        public void setStorage(String storage) {
+            this.storage = storage;
+        }
+
+        public int getStock() {
+            return stock;
+        }
+
+        public void setStock(int stock) {
+            this.stock = stock;
+        }
+    }
+
+    public class Phone extends Index.Product {
+        private String color;
+        private String screenResolution;
+        private String cameraResolution;
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+
+        public String getScreenResolution() {
+            return screenResolution;
+        }
+
+        public void setScreenResolution(String screenResolution) {
+            this.screenResolution = screenResolution;
+        }
+
+        public String getCameraResolution() {
+            return cameraResolution;
+        }
+
+        public void setCameraResolution(String cameraResolution) {
+            this.cameraResolution = cameraResolution;
+        }
+    }
+
+    public class CPU extends Index.Product {
+        private String processor;
+        private String ram;
+        private String operatingSystem;
+        private String graphicsCard;
+
+        public String getProcessor() {
+            return processor;
+        }
+
+        public void setProcessor(String processor) {
+            this.processor = processor;
+        }
+
+        public String getRam() {
+            return ram;
+        }
+
+        public void setRam(String ram) {
+            this.ram = ram;
+        }
+
+        public String getOperatingSystem() {
+            return operatingSystem;
+        }
+
+        public void setOperatingSystem(String operatingSystem) {
+            this.operatingSystem = operatingSystem;
+        }
+
+        public String getGraphicsCard() {
+            return graphicsCard;
+        }
+
+        public void setGraphicsCard(String graphicsCard) {
+            this.graphicsCard = graphicsCard;
+        }
+    }
+
+    public class Console extends Index.Product {
+        @SerializedName("features")
+        private List <String> features;
+        private String color;
+
+        public List <String> getFeatures() {
+            return features;
+        }
+
+        public void setFeatures(List<String> features) {
+            this.features = features;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+    }
+
+    public class FeaturesDeserializer implements JsonDeserializer<List<String>> {
+        @Override
+        public List<String> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            List<String> featuresList = new ArrayList<>();
+            if (json.isJsonArray()) {
+                JsonArray jsonArray = json.getAsJsonArray();
+                for (JsonElement element : jsonArray) {
+                    featuresList.add(element.getAsString());
+                }
+            } else if (json.isJsonPrimitive()) {
+                featuresList.add(json.getAsString());
+            }
+            return featuresList;
+        }
+    }
+
+    public class ProductResponseList {
+        @SerializedName("cellphones")
+        private List<Index.Phone> phones;
+
+        @SerializedName("computers")
+        private List<Index.CPU> cpus;
+
+        @SerializedName("gconsoles")
+        private List<Index.Console> consoles;
+
+        public List<Index.Phone> getPhones() {
+            return phones;
+        }
+
+        public void setPhones(List<Index.Phone> phones) {
+            this.phones = phones;
+        }
+
+        public List<Index.CPU> getCpus() {
+            return cpus;
+        }
+
+        public void setCpus(List<Index.CPU> cpus) {
+            this.cpus = cpus;
+        }
+
+        public List<Index.Console> getConsoles() {
+            return consoles;
+        }
+
+        public void setConsoles(List<Index.Console> consoles) {
+            this.consoles = consoles;
+        }
+    }
 
 
     @Override
